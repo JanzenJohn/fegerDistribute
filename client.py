d43c3c3d8784f8b95d3fd713b4b0cf0f3705d10b
johann <work@johnjanzen.me>
2023-04-30 02:35:46 +0200

    1.2

--- client.py
+++ client.py
@@ -1,14 +1,32 @@
 import websockets
 import asyncio
 import requests
+import argparse
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--port", type=int, default=8765)
+argparser.add_argument("--host", type=str, default="localhost")
+argparser.add_argument("--ws-ssl", type=bool, default=False)
+argparser.add_argument("--sd-port", type=int, default=7860)
+argparser.add_argument("--sd-host", type=str, default="localhost")
+argparser.add_argument("--sd-ssl", type=bool, default=False)
+args = argparser.parse_args()
+
+websocket_url = "wss://" if args.ssl else "ws://"
+websocket_url += f"{args.host}:{args.port}"
+
+sd_url = "https://" if args.sd_ssl else "http://"
+sd_url += f"{args.sd_host}:{args.sd_port}"
+
 
 async def main():
-    async with websockets.connect("ws://localhost:8765") as websocket:
+    
+    async with websockets.connect(websocket_url) as websocket:
         while True:
             try:
                 await websocket.send("NEED_JOB")
                 job_id, job_data = (await websocket.recv()).split("::")
-                x = requests.post("http://localhost:7860/sdapi/v1/txt2img", json={"prompt": job_data}) 
+                x = requests.post(f"{sd_url}/sdapi/v1/txt2img", json={"prompt": job_data}) 
                 if x.status_code != 200:
                     raise Exception(f"Error: {x.status_code}")
                 await websocket.send(f"DONE_JOB::{job_id}::{x.json()['images'][0]}")
