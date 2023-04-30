4e1930fe4f3e78430460640915b765bccd401757
johann <work@johnjanzen.me>
2023-04-30 22:03:47 +0200

    add retries

--- client.js
+++ client.js
@@ -15,29 +15,46 @@ let master_url = args.master_ssl ? "wss://" : "ws://"
 master_url += args.master_host + ":" + args.master_port
 let sd_url = args.sd_ssl ? "https://" : "http://"
 sd_url += args.sd_host + ":" + args.sd_port
+let retires = 0
 
-x = new WebSocket(master_url);
+function create() {
+    console.log("TRYING TO CONNECT")
+    x = new WebSocket(master_url);
 
-x.onerror = function (err) {
-    console.log(err);
-}
-x.onmessage = async function (msg) {
-    let [id, data] = msg.data.split("::")
-    console.log({ id, data })
-    const resp = await fetch(sd_url + "/sdapi/v1/txt2img", { body: JSON.stringify({ prompt: data }), method: "POST", headers: { "Content-Type": "application/json" } })
-    console.log("got resp")
-    if (resp.status == 200) {
-        console.log("went good")
-        await x.send("DONE_JOB::" + id + "::" + JSON.parse(await resp.text())["images"][0])
-    } else {
-        console.log(`wend bad ` + resp.status)
-        await x.send("ERROR_JOB::" + id + "::" + "idk")
+    x.onerror = function () {
+        console.log("ERROR WHILE CONNECTING")
     }
-    await x.send("NEED_JOB")
+    x.onclose = function () {
+        retires += 1
+        if (retires < 30) {
+            setTimeout(create, 5000)
+        } else {
+            console.log("giving up")
+            process.exit(1)
+        }
+    }
+    x.onmessage = async function (msg) {
+        let [id, data] = msg.data.split("::")
+        console.log({ id, data })
+        const resp = await fetch(sd_url + "/sdapi/v1/txt2img", { body: JSON.stringify({ prompt: data }), method: "POST", headers: { "Content-Type": "application/json" } })
+        console.log("got resp")
+        if (resp.status == 200) {
+            console.log("went good")
+            await x.send("DONE_JOB::" + id + "::" + JSON.parse(await resp.text())["images"][0])
+        } else {
+            console.log(`wend bad ` + resp.status)
+            await x.send("ERROR_JOB::" + id + "::" + "idk")
+        }
+        await x.send("NEED_JOB")
 
+    }
+
+    x.onopen = function () {
+        console.log("connected")
+        x.send("NEED_JOB")
+        retires = 0
+    }
 }
 
-x.onopen = function () {
-    console.log("connected")
-    x.send("NEED_JOB")
-}
+
+create()
